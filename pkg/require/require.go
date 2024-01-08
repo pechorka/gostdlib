dfac5b4bee5659bf80e7bca414a1c0c47c364320
pechorka <pechor44@gmail.com>
2024-01-08 13:51:15 +0300

    err assertion for require package

--- pkg/require/require.go
+++ pkg/require/require.go
@@ -1,8 +1,10 @@
 package require
 
-import "testing"
+import (
+	"testing"
+)
 
-// Equal is a helper for comparing to variables in non deep-equal way
+// Equal is a helper for comparing 2 variables in non deep-equal way
 func Equal[T comparable](t *testing.T, expect, got T) {
 	if expect != got {
 		logDiff(t, expect, got)
@@ -10,12 +12,26 @@ func Equal[T comparable](t *testing.T, expect, got T) {
 	}
 }
 
-// EqualWithMessage is a helper for comparing to variables in non deep-equal way.
+// EqualWithMessage is a helper for comparing 2 variables in non deep-equal way.
 // Will print provided message
 func EqualWithMessage[T comparable](t *testing.T, expect, got T, msg string, args ...any) {
 	if expect != got {
 		logDiff(t, expect, got)
 		t.Fatalf(msg, args...)
+	}
+}
+
+// Error is a helper for ensuring that error is not nil
+func Error(t *testing.T, err error) {
+	if err == nil {
+		t.Fatal("\nExpected error")
+	}
+}
+
+// NoError is a helper for ensuring that error is nil
+func NoError(t *testing.T, err error) {
+	if err != nil {
+		t.Fatalf("\nExpected no error, got: %v", err)
 	}
 }
 
